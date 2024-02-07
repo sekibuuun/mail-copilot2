a9dd7c9ba5afb154a548b75a4f94cbc731357266
Aoyama Kazuki <sekibun3109@gmail.com>
2024-02-08 00:10:30 +0900

    chore:  メール本文の取得がループ

--- src/pages/content/ui/app.tsx
+++ src/pages/content/ui/app.tsx
@@ -1,9 +1,44 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function App() {
+function App() {
+  const [isLogged, setIsLogged] = useState<boolean>(false);
   useEffect(() => {
-    console.log('content view loaded');
-  }, []);
+    const observer = new MutationObserver(mutationsList => {
+      mutationsList.forEach(mutation => {
+        if (mutation.type === 'childList' && !isLogged) {
+          const element = document.querySelector('.Am.aiL.Al.editable.LW-avf.tS-tW');
+          if (element) {
+            element.addEventListener('click', () => {
+              console.log('Element was clicked');
+            });
+            setIsLogged(true);
+          }
+        }
+      });
+      // for (const mutation of mutationsList) {
+      //   if (mutation.type === 'childList') {
+      //     const element = document.querySelector('.Am.aiL.Al.editable.LW-avf.tS-tW');
+      //     if (element) {
+      //       element.addEventListener('click', () => {
+      //         console.log('Element was clicked');
+      //       });
+      //     }
+      //   }
+      // }
+    });
 
-  return <div className="">content view</div>;
+    //  ドキュメントに設定されたパラメータで観察を開始
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    //  コンポーネントがアンマウントされたときにオブザーバーを切断
+    return () => observer.disconnect();
+  }, [isLogged]);
+
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
 }
+
+export default App;

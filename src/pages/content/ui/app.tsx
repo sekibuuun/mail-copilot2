8ded0e1a3d1dc1fe094335aadc38597c51174138
Aoyama Kazuki <sekibun3109@gmail.com>
2024-02-08 13:59:33 +0900

    feat: 本文入力カーソルに行くと、"aaa"が入力される

--- src/pages/content/ui/app.tsx
+++ src/pages/content/ui/app.tsx
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
-  const [isLogged, setIsLogged] = useState<boolean>(false);
   useEffect(() => {
     const observer = new MutationObserver(mutationsList => {
       mutationsList.forEach(mutation => {
-        if (mutation.type === 'childList' && !isLogged) {
+        if (mutation.type === 'childList') {
           const element = document.querySelector('.Am.aiL.Al.editable.LW-avf.tS-tW');
           if (element) {
             element.addEventListener('click', () => {
               console.log('Element was clicked');
+              // ここに関数を書く
+              element.textContent = 'aaa'; // Set the text content to 'aaa'
+              console.log(localStorage.getItem('apiKey'));
+              // optionsにあるものをpopupに移動させ、apikeyが取得できるか確認する
             });
-            setIsLogged(true);
           }
         }
       });
@@ -32,13 +34,9 @@ function App() {
 
     //  コンポーネントがアンマウントされたときにオブザーバーを切断
     return () => observer.disconnect();
-  }, [isLogged]);
+  }, []);
 
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

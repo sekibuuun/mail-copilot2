09603abf87c134acbdf9b1e8857288fb8cbf8013
Aoyama Kazuki <sekibun3109@gmail.com>
2024-03-22 21:39:13 +0900

    feat: 本文から件名生成

--- src/pages/content/ui/app.tsx
+++ src/pages/content/ui/app.tsx
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [judgeMode, setJudgeMode] = useState<number>(2); // 1: 本文生成, 2: 件名生成
+
   useEffect(() => {
-    const handleClick = async event => {
+    const textHandleClick = async event => {
       const element = event.target.closest('.Am.aiL.Al.editable.LW-avf.tS-tW');
       if (element) {
-        console.log('Element was clicked');
+        setJudgeMode(2);
+        console.log('本文がクリックされた');
         const apiKey = await chrome.storage.sync.get(['key']).then(result => result.key);
+        const subjectInput = document.getElementsByName('subjectbox')[0] as HTMLInputElement;
+        const subject = subjectInput.value;
 
-        getSuggest(element, apiKey);
+        getSuggest(element, apiKey, judgeMode, subject);
 
         element.addEventListener('keydown', function (e) {
           if (e.shiftKey && e.key === 'Enter') {
@@ -17,7 +22,7 @@ function App() {
             console.log('shift + Enterが押された');
             // サジェストを決定
             element.style.color = 'black';
-            document.removeEventListener('click', handleClick);
+            document.removeEventListener('click', textHandleClick);
           }
           if (e.shiftKey && e.key === 'Backspace') {
             e.preventDefault();
@@ -26,43 +31,88 @@ function App() {
             element.textContent = '';
             element.style.color = 'black';
 
-            getSuggest(element, apiKey);
+            getSuggest(element, apiKey, judgeMode, subject);
           }
         });
       }
     };
 
     // ドキュメント全体に対して一度だけイベントリスナーを追加
-    document.addEventListener('click', handleClick);
+    document.addEventListener('click', textHandleClick);
 
     // コンポーネントがアンマウントされたときにイベントリスナーを削除
-    return () => document.removeEventListener('click', handleClick);
-  }, []);
+    return () => document.removeEventListener('click', textHandleClick);
+  }, [judgeMode]);
+
+  useEffect(() => {
+    const subjectHandleClick = async event => {
+      const element = event.target.closest('.aoT');
+      if (element) {
+        setJudgeMode(1);
+        const textInput = document.querySelectorAll('.Am.aiL.Al.editable.LW-avf.tS-tW');
+        const text = textInput[0].textContent;
+        console.log('本文：' + text);
+        console.log('件名がクリックされた');
+
+        const apiKey = await chrome.storage.sync.get(['key']).then(result => result.key);
+
+        getSuggest(element, apiKey, judgeMode, text);
+
+        element.addEventListener('keydown', function (e) {
+          if (e.shiftKey && e.key === 'Enter') {
+            e.preventDefault();
+            console.log('shift + Enterが押された');
+            // サジェストを決定
+            element.style.color = 'black';
+            document.removeEventListener('click', subjectHandleClick);
+          }
+          if (e.shiftKey && e.key === 'Backspace') {
+            e.preventDefault();
+            console.log('shift + Backspaceが押された');
+            // サジェストを削除
+            element.textContent = '';
+            element.style.color = 'black';
+
+            getSuggest(element, apiKey, judgeMode, text);
+          }
+        });
+      }
+    };
+
+    // ドキュメント全体に対して一度だけイベントリスナーを追加
+    document.addEventListener('click', subjectHandleClick);
+
+    // コンポーネントがアンマウントされたときにイベントリスナーを削除
+    return () => document.removeEventListener('click', subjectHandleClick);
+  }, [judgeMode]);
 
   return <div></div>;
 }
 
-function getSuggest(element, apiKey) {
-  // ここにGETリクエストを送る処理を書いて
-  const output = postSubject(element, apiKey);
+function getSuggest(element, apiKey, judgeMode, input) {
+  console.log(judgeMode);
 
-  // サジェストを表示
-  element.textContent = output;
+  if (judgeMode === 1) {
+    const text = postSubject(element, apiKey, judgeMode, input);
+    element.textContent = text;
+  }
+  if (judgeMode === 2) {
+    const subject = postText(apiKey, judgeMode, input);
+    element.textContent = subject;
+  }
+  // ここにGETリクエストを送る処理を書いて
   element.style.color = 'gray';
 
   return element;
 }
 
-function postSubject(element: Element, apiKey: string) {
-  const subjectInput = document.getElementsByName('subjectbox')[0] as HTMLInputElement;
-  const subject = subjectInput.value;
-  console.log(subject);
-
-  const apply = (subject: string) => {
+function postSubject(element: Element, apiKey: string, judgeMode: number, subject: string) {
+  const applyText = (subject: string, judgeMode: number) => {
     if (subject === '') {
       alert('件名を入力してください');
     }
-    const url = `http://127.0.0.1:3000/mail_copilot/${apiKey}/${subject}`;
+    const url = `http://127.0.0.1:3000/mail_copilot/${apiKey}/${subject}/${judgeMode}`;
+    console.log(url);
     axios
       .get(url)
       .then(response => {
@@ -89,7 +139,57 @@ function postSubject(element: Element, apiKey: string) {
       });
   };
 
-  const response = apply(subject);
+  const response = applyText(subject, judgeMode);
+
+  return response;
+}
+
+function postText(apiKey: string, judgeMode: number, text: string) {
+  // const getTextRecursive = node => {
+  //   let text = '';
+  //   const processNode = n => {
+  //     for (let child of n.childNodes) {
+  //       if (child.nodeType === Node.TEXT_NODE) {
+  //         text += child.textContent;
+  //       } else if (child.nodeType === Node.ELEMENT_NODE) {
+  //         processNode(child);
+  //       }
+  //     }
+  //   };
+  //   processNode(node);
+  //   return text;
+  // };
+
+  const applyText = (text: string, judgeMode: number) => {
+    if (text === '') {
+      alert('本文を入力してください');
+    }
+    const url = `http://127.0.0.1:3000/mail_copilot/${apiKey}/${text}/${judgeMode}`;
+    console.log(url);
+
+    axios
+      .get(url)
+      .then(response => {
+        const subjectInput = document.getElementsByName('subjectbox')[0] as HTMLInputElement;
+        console.log(response);
+        const lines = response.data.split('\n');
+        const subjectLine = lines.find(line => line.trim() !== '');
+        if (subjectLine && subjectInput) {
+          subjectInput.value = subjectLine;
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        if (error.message === 'Network Error') {
+          alert('サーバーに接続できませんでした');
+        }
+        if (error.message === 'Request failed with status code 500') {
+          alert('APIキーが正しくありません');
+        }
+      });
+  };
+
+  const response = applyText(text, judgeMode);
 
   return response;
 }
